0eaa07b94c8158d4096f2ad416d432a2812a73fc
Gyusung Lee <ko76562385@gmail.com>
2025-06-14 21:58:14 +0900

    multiple texture meterial

--- study/js/04-material.js
+++ study/js/04-material.js
@@ -1,5 +1,6 @@
 import * as THREE from "https://unpkg.com/three@0.150.0/build/three.module.js";
 import {OrbitControls} from "https://unpkg.com/three@0.150.0/examples/jsm/controls/OrbitControls.js";
+// import {VertexNormalsHelper} from "https://unpkg.com/three@0.150.0/examples/jsm/helpers/VertexNormalsHelper.js";
 
 class App {
   constructor() {
@@ -35,35 +36,63 @@ class App {
     const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
     camera.position.z = 3;
     this._camera = camera;
+    this._scene.add(camera);
   }
 
   _setupLight() {
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    this._scene.add(ambientLight);
+
     const color = 0xffffff;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1, 2, 4);
-    this._scene.add(light);
+    // this._scene.add(light);
+    this._camera.add(light);
   }
 
   _setupModel() {
     const textureLoader = new THREE.TextureLoader();
-    const map = textureLoader.load(
-      "https://raw.githubusercontent.com/mrdoob/three.js/r150/examples/textures/uv_grid_opengl.jpg",
-      (texture) => {
-        texture.repeat.x = 1;
-        texture.repeat.y = 1;
-      },
-    );
+    const map = textureLoader.load("../../images/glass/Glass_Window_002_basecolor.jpg");
+    const mapAO = textureLoader.load("../../images/glass/Glass_Window_002_ambientOcclusion.jpg");
+    const mapHeight = textureLoader.load("../../images/glass/Glass_Window_002_height.png");
+    const mapNormal = textureLoader.load("../../images/glass/Glass_Window_002_normal.jpg");
+    const mapRoughness = textureLoader.load("../../images/glass/Glass_Window_002_roughness.jpg");
+    const mapMetalic = textureLoader.load("../../images/glass/Glass_Window_002_metallic.jpg");
+    const mapAlpha = textureLoader.load("../../images/glass/Glass_Window_002_opacity.jpg");
+    // const mapLight = textureLoader.load("../../images/glass/light.jpg");
     const material = new THREE.MeshStandardMaterial({
       map: map,
+
+      normalMap: mapNormal,
+      displacementMap: mapHeight,
+      displacementScale: 0.2,
+      displacementBias: -0.15,
+
+      aoMap: mapAO,
+      aoMapIntensity: 1,
+
+      roughnessMap: mapRoughness,
+      roughness: 0.5,
+
+      metalnessMap: mapMetalic,
+      metalness: 0.5,
+
+      //alphaMap: mapAlpha,
+      transparent: true,
+      side: THREE.DoubleSide,
+      // lightMap: mapLight,
+      // lightMapIntensity: 1,
     });
 
-    const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
+    const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1, 256, 256, 256), material);
     box.position.set(-1, 0, 0);
+    box.geometry.attributes.uv2 = box.geometry.attributes.uv;
     this._scene.add(box);
 
-    const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.7, 32, 32), material);
+    const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.7, 512, 512), material);
     sphere.position.set(1, 0, 0);
+    sphere.geometry.attributes.uv2 = box.geometry.attributes.uv;
     this._scene.add(sphere);
   }
 
